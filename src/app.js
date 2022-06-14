be4a2c52ea0f1029cd61a765059ba7ad181e661c
camilamasson321 <camilamasson321@gmail.com>
2022-06-14 00:57:45 +0000

    draft

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let numbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "K",
+  "Q",
+  "J",
+  "A"
+];
+let suit = ["♦", "♥", "♠", "♣"];
+
+function getRandomInt(array) {
+  return Math.floor(Math.random() * array.length);
+}
+const randomNumber = numbers[getRandomInt(numbers)];
+const randomSuit = suit[getRandomInt(suit)];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".topSuit").innerHTML = randomSuit;
+  document.querySelector(".number").innerHTML = randomNumber;
+  document.querySelector(".bottomSuit").innerHTML = randomSuit;
 };
+
+let changeColor = suit[(0, 1)];
+if (changeColor) {
+  document.getElementById(changeColor).style.color = `red`;
+}
